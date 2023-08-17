5cb2c32a1ac61e7eae5d43a675d7971e766248cf
bladimirh179@gmail.com <bladimirh179@gmail.com>
2023-08-17 09:01:18 -0600

    usamos cowsay

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const {frutas, money} = require('./frutas')
+const cowsay = require('cowsay')
+
+console.log(cowsay.say({
+    text : "Hello. How are you?",
+    e : "OO",
+    T : "U "
+}));
+
+/*const {frutas, money} = require('./frutas')
 
 frutas.forEach(fruta => {
     console.log('La fruta es ', fruta)
 })
 
 console.log('Dinero: ', money)
+*/
